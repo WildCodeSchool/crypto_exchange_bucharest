75bfb5ec94fee7bbcf39bf287fcbc808c5467e4c
S0nicTheHedgehog <notyorphobia@gmail.com>
2019-11-19 18:16:46 +0200

    navswitch not redy

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import {
   BrowserRouter,
   Redirect,
   Switch,
-  NavLink
+  NavLink,
 } from 'react-router-dom';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 
 
 import MainPage from './login-page'
 import './App.css';
+import NavSwitch from './Switch';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -30,12 +33,34 @@ class App extends React.Component {
       <div className="App">
         {this.state.loginVisible ?
           (<MainPage nume={this.isLoginVisible} />)
-          : <h1>YEEEEEEEEEEEEe</h1>}
+          :
+          <div>
+          <Navbar expand="lg">
+            <Navbar.Brand href="#home"><img id="myGoat" alt='' src={this.props.goat} /></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <div class="font ml-auto" >
+                <Nav>
+                  <Nav.Link href="markets"><h4 className={this.props.dark ? 'light-color' : 'dark-color'}>Markets</h4></Nav.Link>
+                  <Nav.Link href="orders"><h4 className={this.props.dark ? 'light-color' : 'dark-color'}>Orders</h4></Nav.Link>
+                  <Nav.Link href="wallet"><h4 className={this.props.dark ? 'light-color' : 'dark-color'}>Wallet</h4></Nav.Link>
+                  <Nav.Link href="log-in"><h4 className={this.props.dark ? 'light-color' : 'dark-color'}>Log In</h4></Nav.Link>
+                </Nav>
+              </div>
+            </Navbar.Collapse>
+          </Navbar>
 
-        {/* <BrowserRouter>
+        <div className={this.state.dark ? 'dark' : 'light'}>                 
 
-        </BrowserRouter> */}
+          <NavSwitch dark={this.state.dark} handle={this.handleClick} />
+
+        </div>
+        </div>
+        }
+
+
         
+            
       </div>
     );
   }
---
+++ src/Switch.js
@@ -0,0 +1,33 @@
+
+import React, { Component } from 'react';
+import './Switch.css'
+import { Form } from 'react-bootstrap';
+
+class NavSwitch extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dark: this.props.dark,
+            handle: this.props.handle
+        }
+    }
+    handleClick = () => {
+        this.setState({ dark: !this.state.dark })
+    }
+    render() {
+
+        return (
+            <Form className={`switchdiv ${this.props.dark ? 'dark' : 'light'}`}>
+                <Form.Check
+                    type="switch"
+                    id="custom-switch"
+                    label=""
+                    onClick={this.props.handle}
+                />
+            </Form>
+        )
+    }
+}
+
+export default NavSwitch;
